6ec9d98eed80920185d65346703c652c1263ffb4
izuminanami <37imuzi@gmail.com>
2025-04-22 12:41:58 +0900

    Initial Commit

---
+++ ESHub/ESHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESHubApp.swift
+//  ESHub
+//
+//  Created by 泉七海 on 2025/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ESHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

ba1a3fd4295077e7c40a5f067ee813fac08442b4
izuminanami <37imuzi@gmail.com>
2025-05-03 14:19:00 +0900

    ライブ名と合言葉が設定されたものと一致した場合のみバンドリストを表示するよう実装

--- ESHub/Views/ReceiveLoginView.swift
+++ ESHub/Views/ReceiveLoginView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ReceiveLoginView: View {
+    @StateObject private var spreadSheetManager = LiveSheetManager()
+    @State private var isAuthorized = false
+    @State private var showAlert = false
     @State var liveName = ""
     @State var watchWord = ""
     var body: some View {
@@ -17,27 +20,40 @@ struct ReceiveLoginView: View {
                     .edgesIgnoringSafeArea(.all)
                 VStack(spacing: 10) {
                     Text("ライブ名")
+                        .font(.title2)
                     TextField("ライブ名を入力してください", text: $liveName)
                         .textFieldStyle(.roundedBorder)
                         .padding()
                     
                     Text("作成したライブ名を入力してください")
+                        .font(.subheadline)
                         .foregroundColor(.gray)
                         .padding()
                     
-                    Text("あいことば")
-                    TextField("あいことばを入力してください", text: $watchWord)
+                    Text("合言葉")
+                        .font(.title2)
+                    TextField("合言葉を入力してください", text: $watchWord)
                         .textFieldStyle(.roundedBorder)
                         .padding()
                     
-                    Text("設定したあいことばを入力してください")
+                    Text("設定した合言葉を入力してください")
+                        .font(.subheadline)
                         .foregroundColor(.gray)
                         .padding()
                     
-                    NavigationLink {
-                        ReceiveListView(liveName: liveName)
+                    Button {
+                        if spreadSheetManager.spreadSheetResponse.values.contains(where: { $0[0] == liveName && $0[1] == watchWord }) {
+                            isAuthorized = true
+                        } else {
+                            showAlert = true
+                        }
                     } label: {
-                        SmallButtonLabelComponent(text: "ログイン")
+                        SmallButtonLabelComponent(text: "表示")
+                    }
+                    .navigationDestination(isPresented: $isAuthorized) {
+                        ReceiveListView(liveName: liveName)}
+                    .alert("ライブ名または合言葉が正しくありません", isPresented: $showAlert) {
+                        Button("OK", role: .cancel) {}
                     }
                 }
                 VStack {
@@ -47,6 +63,14 @@ struct ReceiveLoginView: View {
                 }
             }
             .hideKeyboardOnTap()
+            .task {
+                do {
+                    try await spreadSheetManager.fetchGoogleSheetData()
+                    print("success")
+                } catch {
+                    print("error: \(error)")
+                }
+            }
         }
         .navigationTitle("ログイン")
     }
